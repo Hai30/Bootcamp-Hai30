b3c6ef0cfd226674166e37b50a9b01284c7d55e6
Hai <hai1361988@gmail.com>
2020-04-13 06:32:56 +0300

    JS Exercises 2

---
+++ JS/Conditional Statements/main.js
@@ -0,0 +1,86 @@
+
+//Exercise1:
+//==========
+console.log("*Exercise1*")
+console.log("===========")
+let numChildren = prompt("Please enter children number: ")
+let isCareful = false;
+isCareful = prompt("Enter true if you are careful or false if not: ")
+// if (isCareful != true || isCareful != false){
+//     isCareful = prompt("Wrong value! Please enter true if you are careful or false if not: ")
+// }
+if (isCareful == false){
+    numChildren++
+} 
+console.log("numChildren is: "+numChildren)
+
+
+// Exercise2:
+//===========
+console.log("*Exercise2*")
+console.log("===========")
+ let silverwareCount = prompt("Please enter a number:")
+ if (silverwareCount % 2 == 0){
+     console.log("Everything is ok!!")
+ } else {
+     console.log("Something missing")
+ }
+
+
+
+ // Exercise3:
+//===========
+console.log("*Exercise3*")
+console.log("===========")
+ let silverwareCount = prompt("Please enter a number:")
+ if (silverwareCount % 2 == 0){
+     console.log("Everything is ok!!")
+ } else {
+     console.log("Something missing")
+ }
+
+// const myVariable = "The potential of the future "
+// const result = myVariable + "This is the best thing ever"
+// console.log(result)
+
+// Exercise3:
+//===========
+
+// const password = "abc123"
+// const confirmPassword = "a123456"
+// if (password == confirmPassword){
+//     console.log("you successfully login")
+// } else console.log("The password you entered in wrong!")
+
+
+// Exercise4:
+//===========
+
+// console.log(432 * 12)
+// console.log(802 / 2)
+// console.log((5+6)*3)
+
+
+// Exercise5:
+//===========
+
+// console.log((5 > 2) && false)
+
+// console.log(!["knife" === "sword"])
+
+// console.log((1 < 2) || ( -1 > -1 ) || !false)
+
+// console.log("")
+
+// console.log((31 % 5) == "1")
+
+// console.log(!true)
+
+// console.log("5th Avenue" != "5th Avenue")
+
+// console.log(52 !== "52")
+
+// console.log(undefined || null)
+
+
+
