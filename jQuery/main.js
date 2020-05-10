4b0e951ad3e341f8efafedde00a66defb9c3869b
Hai <hai1361988@gmail.com>
2020-05-10 21:19:10 +0300

    jQuery

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,51 +1,268 @@
-//DOM
+// jQuery:
+// =======
+// console.log($)
+// console.log(jQuery)
+// $("h4").css("color", "red")
 
-//Exercise 3:
-//============
 
-const getRandomColor = function() {
-    const niceColors = ["fa744f", "fa744f","#8e44ad", "#3498db", "#c0392b", "#f1c40f", "#d35400", "#2ecc71", "#1abc9c", "#2c3e50", "#7f8c8d", "#862a5c"]
+// $('#my-input').val()
+
+// $('button').on('click', function () {
+//     alert('clicked!')
+//   })
+
+//   $('button').click(function () {
+//     alert('clicked!')
+//   })
     
-    const randomPosition = Math.floor(Math.random() * niceColors.length);
-    return niceColors[randomPosition];
-  }
 
-  const colors = []
+// $("#b1").hover(function () {
+//     console.log($(this))
+// })
 
-  const checkIfAllTheSame = function() {
-    let color = colors[0]
-    for(let i in colors) {
-        if (colors[i]!=color) return
-    }
-    const win = document.createElement('h1')
-    win.innerHTML='All the Colors fit!'
-    win.style.fontSize = 'x-large'
-    win.style.position = 'absolute'
-    win.style.bottom = 0
-    win.style.left = '250px'
-    win.style.color = 'Red'
-    document.getElementById('container').appendChild(win)
-}
+// const aDynamicDiv = "<div class='mine'>Oh yes</div>"
+// const elem = $(aDynamicDiv)
+// console.log(elem)
+
+// $("body").append(elem)
+
+// $('.fruits').append('<p class="red">Raspberry</p>')
+// $('.fruits').append('<p class="brown">Kiwi</p>')
+
+// const text = "Banana"
+// const item = $("<div class=fruit>" + text + "</div>")
+
+// console.log(item) //prints <div class=fruit>Banana</div> as a jQuery object - this is what we created!
 
 
-let left = 0 
-for(let i=0;i<6;i++) {
-    const box = document.createElement('div')
-    box.setAttribute('id','box' + i)
-    box.setAttribute('class','box')
-    box.setAttribute('onmouseenter',"changeColor(this,"+i+ ")")
-    box.style.left += (left + 'px')
-    left+=105 
-    let color = getRandomColor() 
-    colors[i] = color
-    box.style.backgroundColor = color
-    document.getElementById('container').appendChild(box)
-}
+// const names = ["Alex", "Byron", "Cassandra", "Deandre", "Ellena"]
+
+// for(let name of names){
+//   $("body").append(`<div>${name}</div>`)
+// }
 
 
-const changeColor = function (box,i) {
-    let color = getRandomColor()
-    colors[i] = color
-    box.style.backgroundColor = color
-    checkIfAllTheSame()
-}
+// $("p").remove()
+
+// $("p").remove(".brown")
+
+// const addDiv = function() {
+//     $("body").append("<div class=box></div>")
+//   };
+  
+//   $("button").on("click", function() {
+//     addDiv()
+//   });
+  
+//   $(".box").on("click", function() {
+//     console.log("hi")
+//   });
+  
+
+//   const addDiv = function() {
+//     $("body").append("<div class=box></div>");
+    
+//     $(".box").on("click", function() {
+//       alert("hi");
+//     });
+//   };
+  
+//   $("button").on("click", function() {
+//     addDiv();
+//   });
+  
+
+// const addDiv = function() {
+//     $("body").append("<div class=box></div>");
+//   };
+  
+//   $("button").on("click", function() {
+//     addDiv();
+//   });
+  
+//   $("body").on("click", ".box", function() {
+//     alert("hi");
+//   });
+  
+
+
+//Spot Check 2: 
+//-------------
+// $("h1").css("background-color", "blue")
+// $(".red-div").css("background-color", "red")
+// $("li:first-child").css("background-color", "green")
+// $("li:last-child").css("background-color", "pink")
+// $("#brown-div").css("background-color", "brown")
+
+
+//Spot Check 3: 
+//-------------
+// $("#b1").addClass("box")
+// $("#b2").addClass("box")
+
+
+//Spot Check 4: 
+//-------------
+// $("my-input").val("Terabyte")
+
+
+//Spot Check 5: 
+//-------------
+// const data = $("div").data()
+// console.log("Item with barcode " + data.barcode + " will expire on " + data.expirationdate) //notice how we use expirationdate, even though in the HTML it's expirationDate
+
+
+
+//Spot Check :6 
+//-------------
+
+// $("#b1").hover(function(){
+//     $("b1").css("background-color", "blue")
+// })
+
+// $("#b1").hover(function () {
+//     $("#b1").css("background-color", "blue")
+// })
+
+
+
+//Spot Check 7: 
+//-------------
+
+    // $("button").on("click", function () {
+    //     alert($("#my-input").val())
+    // })
+
+
+
+//Spot Check 8: 
+//-------------
+
+// $(".box").hover(function(){
+//     $(this).css("background-color", "blue")
+// })
+
+//Spot Check 9: 
+//-------------
+// $(".feedme").on("click", function(){
+//     let divCopy = `<div class=feedme> ${$(this).text()} </div>`
+    
+//     $("body").append(divCopy)
+//   })
+  
+//Spot Check 10: 
+//-------------
+// const names = [
+//     { first: "Alex", last: "Johnson" },
+//     { first: "Byron", last: "Loveall" },
+//     { first: "Cassandra", last: "Wuthers" },
+//     { first: "Deandre", last: "Rahm" },
+//     { first: "Ellena", last: "Freeman" }
+//   ]
+
+//   for (let name of names) {
+//       $("body").append('<div class=human>${name.first} - ${name.last}</div>');
+      
+//   }
+  
+//   for (let name of names) {
+//     $("body").append(`<div class=human>${name.first} - ${name.last}</div>`);
+//   }
+
+
+
+//Spot Check 11: 
+//-------------
+// $("#plague").hover(function(){
+//     $("#plague").remove()
+// })
+
+
+//Spot Check 12: 
+//-------------
+
+// $("button").on("click", function(){
+//     $("#blogs").append("<div class='blog'>Cool blog</div>")
+//   })
+  
+//   $("#blogs").on("click", ".blog", function(){
+//     $(".blog").text("blargh") 
+//   })
+
+//Spot Check 13: 
+//-------------
+
+
+// $("button").on("click", function(){
+//     $("#blogs").append("<div class='blog'>Cool blog</div>")
+//   })
+  
+//   $("#blogs").on("click", ".blog", function(){
+//     $(this).text("uncool blog") 
+//   })
+
+
+
+
+// Exercise 1:
+// ===========
+// $("button").on("click", function () {
+//     $("#human-list").append(`<li>${$("input").val()}</li>`)
+//     $("input").val("")
+// })
+
+
+
+
+
+// Exercise 2:
+// ===========
+// $("#human-list").on("click", "li", function() {
+//     $(this).remove()
+// })
+
+
+// Exercise 3:
+// ===========
+// $("body").append("<div class=\"box\" id=\"box-1\"></div>")
+// 	$("body").append("<div class=\"box\" id=\"box-2\"></div>")
+	
+// 	let $boxes = $(".box")
+	
+// 	$(".box").hover(function () {
+// 	    $boxes.css("background-color", "rgb(231, 76, 60)")
+// 	    $(this).css("background-color", "rgb(142, 68, 173)")
+// 	})
+
+
+// Exercise 4:
+// ===========
+
+// $(".item").click(function () {
+	
+//     if ($(this).data().instock) {
+//         $("#cart").append(`<div class="cart-item">${$(this).text()}</div>`)
+//     }
+// }) 
+
+
+
+// Exercise 5:
+// ===========
+// const fruits = [
+//     { name: "Orange", color: "orange" },
+//     { name: "Banana", color: "yellow" },
+//     { name: "Coconut", color: "brown" },
+//     { name: "Kiwi", color: "brown" },
+//     { name: "Lemon", color: "yellow" },
+//     { name: "Cucumber", color: "green" },
+//     { name: "Persimmon", color: "orange" },
+//     { name: "Pumpkin", color: "orange" }
+//   ]
+
+//   for(let fruit of fruits) {
+//       $("#basket").append(`<div class="${fruit.color}">${fruit.name}</div>`)
+//   }
+
+
+
